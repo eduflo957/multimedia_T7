ec358a05b7fc2a1dc8a29fb3896bf309d0689cc4
AlumnoM <eduardoflores.9595@gmail.com>
2023-01-20 12:32:55 +0100

    falta linkar las 2 pantallas

---
+++ app/src/main/java/com/example/multimedia_t7/pagina_vacia.kt
@@ -0,0 +1,11 @@
+package com.example.multimedia_t7
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PantallaClases : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.pagina_vacia)
+    }
+}
